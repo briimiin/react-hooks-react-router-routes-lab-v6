1b8126c95753356b7bf69a053dd7040cf8161070
briimiin <kipkorirbrimin005@gmail.com>
2024-05-03 15:27:28 +0300

    react router routes lab

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar"
 
 function Home() {
-  return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
-  );
-};
+const [movies, setMovies] = useState([])
+
+useEffect(() =>{
+  fetch("http://localhost:4000/movies")
+  .then(r => r.json())
+  .then(data => setMovies(data))
+  .catch(error => console.error(error))
+}, [])
+
+const movieList = movies.map(movie => <MovieCard key={movie.id} title={movie.title} id={movie.id}/>)
+
+return (
+  <>
+    <header>
+      <NavBar />
+    </header>
+    <main>
+      <h1>Home Page</h1>
+      {movieList}
+    </main>
+  </>
+);
+}
 
 export default Home;
